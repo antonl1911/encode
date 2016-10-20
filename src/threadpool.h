e95d4c0b4616ca9084e6542ff4759b0dc7df141e
A.Leschenko <antonl1911@gmail.com>
2016-10-20 18:52:46 +0300

    Separate source & headers, some cleanup

---
+++ src/threadpool.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <thread>
+#include <vector>
+#include <iostream>
+#include <pthread.h>
+
+using namespace std;
+
+class ThreadPool {
+    vector<pthread_t> tid;
+	vector<string> *m_queue;
+	static void* ThreadFunction(void *arg);
+	bool (*work_fn)(const string&);
+	size_t ItemsTotal = 0;
+	size_t ItemsCompleted = 0;
+	size_t NumThreads = 0;
+	pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+public:
+    ThreadPool(vector<string>& queue, bool (*fn)(const string&));
+    ThreadPool() = delete;
+    ~ThreadPool();
+};
