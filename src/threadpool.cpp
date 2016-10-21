e4932595850fa156dbcf23b098aab28f70ecca3d
A.Leschenko <antonl1911@gmail.com>
2016-10-21 13:01:36 +0300

    Changed POSIX threads to C++ threads

--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -8,15 +8,14 @@ ThreadPool::ThreadPool(vector<string>& queue, bool (*fn)(const string&)) : m_que
     cout << "Creating " << NumThreads << " threads\n";
     tid.resize(NumThreads);
     for (size_t i = 0; i < NumThreads; i++) {
-        pthread_create(&tid[i], nullptr, ThreadFunction, this);
-        tid.push_back(i);
+        tid[i] = thread(ThreadFunction, this);
     }
 }
 
 ThreadPool::~ThreadPool()
 {
     for (size_t i = 0; i < NumThreads; i++) {
-        pthread_join(tid[i], nullptr);
+        tid[i].join();
     }
     cout << "Successfully processed: " << ItemsCompleted << "/" << ItemsTotal << " items" << '\n';
 }
@@ -26,14 +25,14 @@ void* ThreadPool::ThreadFunction(void *arg)
     auto *ctx = (ThreadPool*)arg;
 
     while (1) {
-        pthread_mutex_lock(&ctx->queue_mutex);
+        ctx->queue_mutex.lock(); 
         if (ctx->m_queue->empty()) {
-            pthread_mutex_unlock(&ctx->queue_mutex);
+            ctx->queue_mutex.unlock(); 
             break;
         }
         string cur (ctx->m_queue->back());
         ctx->m_queue->pop_back();
-        pthread_mutex_unlock(&ctx->queue_mutex);
+        ctx->queue_mutex.unlock();
         if (ctx->work_fn(cur)) // Increase counter on success
             ctx->ItemsCompleted++;
     }
--- src/threadpool.h
+++ src/threadpool.h
@@ -1,20 +1,20 @@
 #pragma once
 #include <thread>
+#include <mutex>
 #include <vector>
 #include <iostream>
-#include <pthread.h>
 
 using namespace std;
 
 class ThreadPool {
-    vector<pthread_t> tid;
+    vector<thread> tid;
     vector<string> *m_queue;
     static void* ThreadFunction(void *arg);
     bool (*work_fn)(const string&);
     size_t ItemsTotal = 0;
     size_t ItemsCompleted = 0;
     size_t NumThreads = 0;
-    pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+    mutex queue_mutex;
 public:
     ThreadPool(vector<string>& queue, bool (*fn)(const string&));
     ThreadPool() = delete;
